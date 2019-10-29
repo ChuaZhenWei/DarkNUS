ba3f93c181a69a101a54715911980893dd73c99b
Lim Woon Hian <35731563+Woonhian@users.noreply.github.com>
2019-10-29 18:56:58 +0800

    Update navigation bar

--- navBar.php
+++ navBar.php
@@ -4,17 +4,17 @@
     </head>
     <body>
         <?php if (!isset($_SESSION['user_role'])){ ?>
-        <a href="index.php">Home</a> | <a href="search.php">Search</a> | <a href="login.php">Login</a> | <a href="register.php">Register</a>
+        <a href="index.php">Home</a> | <a href="login.php">Login</a>
         <?php } 
         else{
             $rol=$_SESSION['user_role'];
             $name=  ucfirst($_SESSION['name']);
             
             if ($_SESSION['user_role']=='Student'){ ?>
-        <a href="index.php">Home</a> | <a href="search.php">Search</a> | <a href="addStory.php">Add New Story</a> | <a href="changePassword.php">Change Password</a> | <a href="doLogout.php">Logout</a>
+        <a href="index.php">Home</a> | <a href="changePassword.php">Change Password</a> | <a href="doLogout.php">Logout</a>
       <?php      }
          elseif($_SESSION['user_role']=='Professor'){ ?>
-             <a href="index.php">Home</a> | <a href="search.php">Search</a> | <a href="showReport.php">Show Report</a> | <a href="changePassword.php">Change Password</a> | <a href="doLogout.php">Logout</a>
+             <a href="index.php">Home</a> | <a href="showReport.php">Show Report</a> | <a href="changePassword.php">Change Password</a> | <a href="doLogout.php">Logout</a>
         <?php }
         echo "<b style='background-color:black; margin-left:50px'><font color='white'>$name ($rol)</font></b>"; 
         }
