c2943c9986f89c9dbac68d2c2ced105f4738fcd8
YiSheng <e0176969@u.nus.edu>
2019-10-31 21:23:34 +0800

    tutorialGroupProf.php included
    Not completed yet.

---
+++ tutorialGroupProf.php
@@ -0,0 +1,18 @@
+<?php
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+?>
+
+<?php
+session_start();
+
+include ('dbFunction.php');
+include ('navBar.php');
+
+
+?>
