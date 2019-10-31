eb502f08c50ac9d082da573f2e6c8539e6197987
YiSheng <e0176969@u.nus.edu>
2019-10-31 20:12:26 +0800

    Update navBar.php to include Tutorial Group

--- navBar.php
+++ navBar.php
@@ -35,7 +35,7 @@
                             <a class="nav-link" href="forum.php">Forum</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <a class="nav-link" href="tutorialGroup.php">Tutorial Group</a>
                         </li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
