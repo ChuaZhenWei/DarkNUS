86d3ed0de217c1f688c4f5d6880c31fc3cd4fe5e
YiSheng <e0176969@u.nus.edu>
2019-10-31 21:05:12 +0800

    Created tutorialGroup.php
    Displaying the student's enrolled tutorial groups

--- tutorialGroup.php
+++ tutorialGroup.php
@@ -1,8 +1,77 @@
 <?php
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+?>
 
+<?php
+session_start();
+
+include ('dbFunction.php');
+include ('navBar.php');
+
+if (!isset($_SESSION['user_id'])) { ?>
+    <h2>Access Denied. User Not Logged In</h2>
+<?php } else {
+
+$id = $_SESSION['user_id'];
+$role = $_SESSION['user_role'];
+
+if ($role == 'Student') {
+    $tutorial = "SELECT courseName, acadYear, sem, tutID, tutDay, startTime, endTime 
+            FROM Belongs B NATURAL JOIN Tutorial_Groups TG
+            WHERE studID = '$id'";
+    
+} elseif ($role == 'Professor') {
+    $query = "SELECT *
+            FROM Tutorial_Groups
+            WHERE profID = '$id'";
+}
+
+debug_to_console($id);
+
+$results = pg_query($tutorial);
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h2>Tutorial Group</h2>
+        <div>
+            <table width="600" border="0" cellpadding="1" cellspacing="1">
+                <tr>
+                    <th>Course Name</th>
+                    <th>Academic Year</th>
+                    <th>Semester</th>
+                    <th>Tutorial ID</th>
+                    <th>Tutorial Day</th>
+                    <th>Tutorial Start Time</th>
+                    <th>Tutorial End Time</th>
+                </tr>
+                <?php
+                while($row = pg_fetch_row($results)) {
+                    echo "<tr>";
+                    echo "<td> $row[0]</td>";
+                    echo "<td> $row[1]</td>";
+                    echo "<td> $row[2]</td>";
+                    echo "<td> $row[3]</td>";
+                    echo "<td> $row[4]</td>";
+                    echo "<td> $row[5]</td>";
+                    echo "<td> $row[6]</td>";
+                    echo "<tr>";
+                }
+                ?>
+            </table>
+        </div>
+    </body>
+</html> 
+<?php
+}
+?>
