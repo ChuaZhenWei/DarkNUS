8a671210be74b389c79775984cbe4d18786133ff
YiSheng <e0176969@u.nus.edu>
2019-11-02 18:39:28 +0800

    remove debug to console

--- requests.php
+++ requests.php
@@ -1,14 +1,4 @@
 <?php
-function debug_to_console($data) {
-    $output = $data;
-    if (is_array($output))
-        $output = implode(',', $output);
-
-    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-}
-?>
-
-<?php
 session_start();
 
 include ('dbFunction.php');
