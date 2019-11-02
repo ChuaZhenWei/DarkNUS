d7a2ace1355535beb85f1efdd413da777f33a846
Zhen Wei <ZhenWeiChua92@gmail.com>
2019-11-02 17:05:51 +0800

    Set redirect
    redirect to login page for user that are not logged in

--- doLogout.php
+++ doLogout.php
@@ -2,15 +2,11 @@
 session_start();
 $message="";
 
-if (!isset($_SESSION['user_id'])) {
-    $message='<p>You are not logged in.</p>';
-}
-else{
+if (isset($_SESSION['user_id'])) {
     $_SESSION = array();
     session_destroy();
-    $message = '<p>You have logged out.</p>';
 }
+header('location:index.php');
 
 include ("navBar.php");
-echo $message;
 ?>
