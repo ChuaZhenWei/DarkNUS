0b42a6c3550da3f4f0f80833ec296de1b198c759
YiSheng <e0176969@u.nus.edu>
2019-11-03 01:05:50 +0800

    Update createThread.php
    Buggy createThread, will fix

--- createThread.php
+++ createThread.php
@@ -7,21 +7,26 @@ include ('navBar.php');
 if (!isset($_SESSION['user_id'])) { ?>
     <h2>Access Denied. User Not Logged In</h2>
 <?php } else {
-
-$id = $_SESSION['user_id'];
-$role = $_SESSION['user_role'];
-
-if ($role == 'Student') {
-    $query = "SELECT *
-        FROM students
-        WHERE studid = '$id'";
+    $forumName = $_GET['fname'];
+    $courseName = $_GET['cname'];
+    $acadYear = $_GET['ay'];
+    $semester = $_GET['sem'];
     
-} elseif ($role == 'Professor') {
-    $query = "SELECT *
-        FROM teaches
-        WHERE profid = '$id'";
-}
+    $id = $_SESSION['user_id'];
+    $role = $_SESSION['user_role'];
+    
+    if (isset($_POST['submit'])) {
+        $threadTitle = $_POST['thread_title'];
+        $postDetail = $_POST['post_content'];
 
+        $insert = "INSERT INTO Threads (courseName, acadYear, sem, forumName, threadTitle, postDetails, userID, posted)
+                    VALUES ('$courseName', $acadYear, $semester, '$forumName', '$threadTitle', 
+                    '$postDetail', '$id', current_timestamp)";
+
+        pg_query($insert);
+    }
+    
+    pg_close();
 ?>
 <html>
     <head>
@@ -33,9 +38,16 @@ if ($role == 'Student') {
             <div class="card">
                 <div class="card-body">                   
                     <h4>Start a Discussion Thread</h4>
-                    <p class="font-weight-light">for "Forum Name"</p>
+                    <p class="font-weight-light">for
+                    <?php
+                        echo $forumName
+                    ?></p>
                     <hr>
-                    <form method="post" action="">
+                    <?php
+                    echo $acadYear;
+                    echo $semester;
+                    echo "<form method='post' action='thread.php?fname=$forumName&cname=$courseName&ay=$acadYear&sem=$semester'>";
+                    ?>
                         <input class="form-control" name="thread_title" type="text" placeholder="Title"><br>
                         <textarea class="form-control" name="post_content" placeholder="Text (required)" rows="5"></textarea><br>
                         <input type="submit" value="Create thread">
