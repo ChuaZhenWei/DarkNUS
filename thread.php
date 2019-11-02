25da9255fa0dcaf00dc23c986b12b01207093c94
Lim Woon Hian <35731563+Woonhian@users.noreply.github.com>
2019-11-02 17:05:34 +0800

    Added createThread.php initial template

--- thread.php
+++ thread.php
@@ -38,7 +38,7 @@ if ($role == 'Student') {
                 </ol>
                 </nav>
                 <nav class="navbar">
-                    <button type="button" class="btn btn-primary">Create New Thread</button>
+                    <a class="btn btn-primary" href="createThread.php" role="button">Create New Thread</a>
                     <form class="form-inline">
                         <input class="form-control mr-sm-2" type="search" placeholder="Search Threads" aria-label="Search">
                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
