a5c350f3011b38d31d43080f2d67b370193bb699
YiSheng <e0176969@u.nus.edu>
2019-11-04 21:17:40 +0800

    Update doCreateThread.php
    Updated redirection of creation for course thread

--- doCreateThread.php
+++ doCreateThread.php
@@ -6,11 +6,11 @@ include ('dbFunction.php');
 if (!isset($_SESSION['user_id'])){
     header('location:index.php');
 } else {
+    $forumName = $_POST['forumName'];
+    $courseName = $_POST['courseName'];
     if (isset($_POST['Action']) && isset($_POST['thread_title']) && isset($_POST['post_content'])) {
         $id = $_SESSION['user_id'];
         
-        $forumName = $_POST['forumName'];
-        $courseName = $_POST['courseName'];
         $acadYear = $_SESSION['acadYear'];
         $semester = $_SESSION['sem'];
         
@@ -21,11 +21,9 @@ if (!isset($_SESSION['user_id'])){
                 VALUES ('$courseName', $acadYear, $semester, '$forumName', '$threadTitle', 
                 '$postDetail', '$id', current_timestamp, NULL)";
         
-        
         pg_query($insert);
-        
-        header("location:thread.php?fname=$forumName&cname=$courseName");
     }
+    header("location:thread.php?fname=$forumName&cname=$courseName");
 }
 pg_close();
 ?>
