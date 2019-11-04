e11c3b6c25cfb743791ea415772f8b49774e8b0e
Zhen Wei <35739423+ChuaZhenWei@users.noreply.github.com>
2019-11-05 02:56:30 +0800

    Add Course to prof

--- addCourse.php
+++ addCourse.php
@@ -6,9 +6,24 @@ include ('navBar.php');
 
 if (!isset($_SESSION['user_id'])) {
     header('location:login.php');
+} else if($_SESSION['user_role'] != 'Professor') {
+    header('location:index.php');
 } else {
     $id = $_SESSION['user_id'];
     $role = $_SESSION['user_role'];
+    $acadYear = $_SESSION['acadYear'];
+    $sem = $_SESSION['sem'];
+    
+    $courses = "SELECT coursename
+        FROM COURSES
+        EXCEPT
+        SELECT coursename
+        FROM TEACHES
+        WHERE profid = '$id'
+        AND acadyear = '$acadYear'
+        AND sem = '$sem'";
+    
+    $result = pg_query($courses);
 ?>
 <html>
     <head>
@@ -21,25 +36,30 @@ if (!isset($_SESSION['user_id'])) {
                 <div class="card-body">                   
                     <h4>Add Course</h4>
                     <hr>
-                    <form class="form-horizontal" method="post" action="index.php">
+                    <form class="form-horizontal" method="post" action="doAddCourse.php">
                         <div class="form-group">
                             <label class="control-label col-sm-2" for="courseName">Course Name:</label>
                             <div class="col-sm-10">
-                                <input class="form-control" name="coursename" type="text" placeholder="Course Name" required>
+                                <select class="form-control" name="coursename" required>
+                                    <option value=''>Select a course</option>
+                                    <?php while($row = pg_fetch_row($result)) {
+                                        echo "<option value='$row[0]'>$row[0]</option>";
+                                    }
+                                    ?>
+                                    
+                                </select>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="faculty">Faculty:</label>
+                            <label class="control-label col-sm-2" for="lectureDay">Lecture Day:</label>
                             <div class="col-sm-10">
-                                <input class="form-control" name="faculty" type="text" placeholder="Faculty" required>
-                            </div>
-                        </div>
-                        <form class="form-horizontal" method="post" action="tutorialGroup.php">
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="lectureday">Lecture Day:</label>
-                            <div class="col-sm-10">
-                                <select class="form-control" name="selected" required>
-                                    <option value="lectureday">Lecture Day</option>
+                                <select class="form-control" name="lectureday" required>
+                                    <option value=''>Select a day</option>
+                                    <option value="Monday">Monday</option>
+                                    <option value="Tuesday">Tuesday</option>
+                                    <option value="Wednesday">Wednesday</option>
+                                    <option value="Thursday">Thursday</option>
+                                    <option value="Friday">Friday</option>
                                 </select>
                             </div>
                         </div>
@@ -53,6 +73,12 @@ if (!isset($_SESSION['user_id'])) {
                             <label class="control-label col-sm-2" for="endtime">Lecture End Time:</label>
                             <div class="col-sm-10">
                                 <input class="form-control" type="time" name="endtime" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="maxHeadCount">Max Head Count:</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" name="maxheadcount" type="text" placeholder="Max Head Count" required>
                             </div>
                         </div>
                         <input type="submit" name="Action" value="Add Course">
---
+++ doAddCourse.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+include ('dbFunction.php');
+
+if (!isset($_SESSION['user_id'])){
+    header('location:index.php');
+} else {
+    if (isset($_POST['Action']) ) {
+        $id = $_SESSION['user_id'];
+        $acadYear = $_SESSION['acadYear'];
+        $sem = $_SESSION['sem'];
+        
+        $courseName = $_POST['coursename'];
+        $day = $_POST['lectureday'];
+        $start = $_POST['starttime'];
+        $end = $_POST['endtime'];
+        $max = $_POST['maxheadcount'];
+        
+        $insert = "INSERT INTO TEACHES (profid, coursename, acadYear, sem, lectureday, starttime, endtime, maxheadcount)
+                VALUES ('$id', '$courseName', $acadYear, $sem, '$day', '$start', '$end', '$max')";
+        
+        pg_query($insert);
+        
+        if (pg_last_notice($link)) {
+            $_SESSION['result'] = pg_last_notice($link);
+            header("location:createForumTutId.php");
+        } else if (pg_last_error($link)) {
+            $_SESSION['result'] = "Already Teaching Course";
+            header("location:createForumTutId.php");
+        }
+        
+        header("location:index.php");
+    }
+}
+pg_close();
+?>
