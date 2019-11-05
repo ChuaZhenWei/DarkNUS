a29669236a01072f86041be89e8e89c615c8210f
Lim Woon Hian <35731563+Woonhian@users.noreply.github.com>
2019-11-05 17:36:43 +0800

    Update forum.php

--- forum.php
+++ forum.php
@@ -42,64 +42,60 @@ if (!isset($_SESSION['user_id'])) {
         <title></title>
     </head>
     <body>
-        <h2>Forum</h2>
-            <div class="card">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item active" aria-current="page">Forum</li>
-                    </ol>
-                </nav>
+        <div class="card">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item active" aria-current="page">Forum</li>
+                </ol>
+            </nav>
+            
+            <div class="card-body">
                 <?php
                 if ($role == 'Professor')
                 {
-                    echo "<nav class='navbar'>";
-                        echo "<ul class='navbar-nav'>";
-                            echo "<li class='nav-item'>";
-                                echo "<a class='btn btn-primary' href='createForumMod.php' role='button'>Create Forum</a>";
-                            echo "</li>";
-                        echo "</ul>";
-                    echo "</nav>";
+                    echo "<a class='btn btn-primary' href='createForumMod.php' role='button'>Create Forum</a>";
+                    echo "<br><br>";
                 }
                 ?>
-                <div class="card-body">
-                    <table cellspacing="1000">
-                        <tbody>
-                            <?php
-                            while ($row = pg_fetch_row($result)) {
-                                $discussion = "
-                                    SELECT COUNT (*) AS threadCount
-                                    FROM 
-                                    (
-                                        SELECT DISTINCT F.forumName, F.courseName, F.acadYear, F.sem, T.threadTitle
-                                        FROM Forums F LEFT JOIN Threads T ON F.courseName = T.courseName
-                                        AND F.acadYear = T.acadYear and F.sem = T.sem AND F.forumName = T.forumName
-                                        WHERE T.threadTitle IS NOT NULL
-                                        GROUP BY F.forumName, F.courseName, F.acadYear, F.sem, T.threadTitle
-                                    ) AS A
-                                    WHERE A.forumName = '$row[0]' AND A.courseName = '$row[1]'
-                                    AND A.acadYear = $row[2] AND A.sem = $row[3]";
-                                $query = pg_query($discussion);
-                                $threadCount = pg_fetch_result($query, 0, 0);
-                                echo "<tr>";
-                                echo "<td>";
-                                echo "<h4><a href='thread.php?fname=$row[0]&amp;cname=$row[1]'>$row[0]</a></h4>";
-                                echo "<p>$row[1]</p>";
-                                echo "<p>Academic Year $row[2], ";
-                                echo "Semester $row[3]</p>"; 
-                                echo "</td>";
-                                echo "<td>";
-                                echo "<p>Number of Discussion Threads: $threadCount</p>";
-                                if ($role == 'Professor') {
-                                    echo "<a href='doDeleteForum.php?fname=$row[0]&amp;cname=$row[1]'><input type = 'button' value = 'Delete' style = 'float : right'></a>";
-                                }
-                                echo "</td>";
-                                echo "</tr>";
+                <table class="table">
+                    <tbody>
+                        <?php
+                        while ($row = pg_fetch_row($result)) {
+                            $discussion = "
+                                SELECT COUNT (*) AS threadCount
+                                FROM 
+                                (
+                                    SELECT DISTINCT F.forumName, F.courseName, F.acadYear, F.sem, T.threadTitle
+                                    FROM Forums F LEFT JOIN Threads T ON F.courseName = T.courseName
+                                    AND F.acadYear = T.acadYear and F.sem = T.sem AND F.forumName = T.forumName
+                                    WHERE T.threadTitle IS NOT NULL
+                                    GROUP BY F.forumName, F.courseName, F.acadYear, F.sem, T.threadTitle
+                                ) AS A
+                                WHERE A.forumName = '$row[0]' AND A.courseName = '$row[1]'
+                                AND A.acadYear = $row[2] AND A.sem = $row[3]";
+                            $query = pg_query($discussion);
+                            $threadCount = pg_fetch_result($query, 0, 0);
+                            echo "<tr>";
+                            echo "<td>";
+                            echo "<h4><a href='thread.php?fname=$row[0]&amp;cname=$row[1]'>$row[0]</a></h4>";
+                            echo "<p>$row[1]</p>";
+                            echo "<p>Academic Year $row[2], ";
+                            echo "Semester $row[3]</p>"; 
+                            echo "</td>";
+                            echo "<td>";
+                            echo "<p>Number of Discussion Threads: $threadCount</p>";
+                            if ($role == 'Professor') {
+                                echo "<br>";
+                                echo "<a href='doDeleteForum.php?fname=$row[0]&amp;cname=$row[1]'><input type = 'button' value = 'Delete' class='btn btn-danger'></a>";
                             }
-                            ?> 
-                        </tbody>
-                    </table>
-                </div>
+                            echo "</td>";
+                            echo "</tr>";
+                        }
+                        ?> 
+                    </tbody>
+                </table>
             </div>
+        </div>
     </body>
 </html> 
 <?php
