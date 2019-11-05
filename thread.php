a06293c6d01566de02e45000f0f269b423ae607a
Lim Woon Hian <35731563+Woonhian@users.noreply.github.com>
2019-11-05 17:51:38 +0800

    Update thread.php

--- thread.php
+++ thread.php
@@ -50,54 +50,53 @@ if (!isset($_SESSION['user_id'])) {
         <title></title>
     </head>
     <body>
-        <h2>Threads</h2>
-            <div class="card">
-                <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="forum.php">Forum</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">
+        <div class="card">
+            <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="forum.php">Forum</a></li>
+                <li class="breadcrumb-item active" aria-current="page">
+                    <?php
+                    echo $forumName;
+                    ?>
+                </li>                    
+            </ol>
+            </nav>
+            <nav class="navbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
                         <?php
-                        echo $forumName;
-                        ?>
-                    </li>                    
-                </ol>
-                </nav>
-                <nav class="navbar">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <?php
-                            echo "<a class='btn btn-primary' href='createThread.php?fname=$forumName&amp;cname=$courseName'
-                                role='button'>Create New Thread</a>";
-                            ?>          
-                        </li>       
-                    </ul>
-                    <form class="form-inline my-2 my-sm-0" method="post" action="">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search Threads" aria-label="Search" name="threadTitle" required>
-                        <button name="searchThread" class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                </nav>
-                <div class="card-body">
-                    <table cellspacing="1000">
-                        <tbody>
-                            <?php
-                            while ($row = pg_fetch_row($results)) {
-                                echo "<tr>";
-                                echo "<td>";
-                                echo "<h4><a href='post.php?fname=$row[1]&amp;cname=$row[0]&amp;threadTitle=$row[2]'>$row[2]</a></h4>";
-                                echo "</td>";
-                                echo "<td>";
-                                echo "<p>Number of replies: $row[3]</p>";
-                                echo "</td>";
-                                if ($role == 'Professor') {
-                                    echo "<a href='doDeleteThread.php?cname=$row[0]&amp;fname=$row[1]&amp;threadTitle=$row[2]'><input type = 'button' value = 'Delete' style = 'float : right'></a>";
-                                }
-                                echo "</tr>";
+                        echo "<a class='btn btn-primary' href='createThread.php?fname=$forumName&amp;cname=$courseName'
+                            role='button'>Create New Thread</a>";
+                        ?>          
+                    </li>       
+                </ul>
+                <form class="form-inline my-2 my-sm-0" method="post" action="">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search Threads" aria-label="Search" name="threadTitle" required>
+                    <button name="searchThread" class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </nav>
+            <div class="card-body">
+                <table class="table">
+                    <tbody>
+                        <?php
+                        while ($row = pg_fetch_row($results)) {
+                            echo "<tr>";
+                            echo "<td>";
+                            echo "<h4><a href='post.php?fname=$row[1]&amp;cname=$row[0]&amp;threadTitle=$row[2]'>$row[2]</a></h4>";
+                            echo "</td>";
+                            echo "<td>";
+                            echo "<p>Number of replies: $row[3]</p>";
+                            if ($role == 'Professor') {
+                                echo "<a href='doDeleteThread.php?cname=$row[0]&amp;fname=$row[1]&amp;threadTitle=$row[2]'><input type = 'button' value = 'Delete' class='btn btn-danger'></a><br><br>";
                             }
-                            ?>
-                        </tbody>
-                    </table>
-                </div>
+                            echo "</td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
             </div>
+        </div>
     </body>
 </html> 
 <?php
