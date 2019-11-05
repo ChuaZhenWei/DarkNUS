1677ad087bd15bd6cf7fbb1c1bdbc4ad15944b10
Lim Woon Hian <35731563+Woonhian@users.noreply.github.com>
2019-11-05 19:17:55 +0800

    Updated createForumMod.php

--- createForumMod.php
+++ createForumMod.php
@@ -27,40 +27,48 @@ if (!isset($_SESSION['user_id'])) {
         <head>
             <meta charset="UTF-8">
             <title></title>
+            <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+            <!-- jQuery library -->
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         </head>
         <body>
-            <h2>Threads</h2>
-                <div class="card">
-                    <div class="card-body">                   
-                        <h4>Create Forum</h4>
-                        <hr>
-                        <form class="form-horizontal" method="post" action="createForumTutId.php">
-                            <div class="form-group">
-                                <label class="control-label col-sm-2" for="courseName">Choose Course:</label>
-                                <div class="col-sm-10">
-                                    <select class="form-control" name="selected" required>
-                                        <option name='coursename' value=''>Select a course</option>
-                                        <?php
-                                        while ($row = pg_fetch_row($result)) {
-                                            echo "<option name='coursename' value='$row[0]'>$row[0]</option>";
-                                        }
-                                        ?>
-                                        
-                                    </select>
-                                </div>
+            <div class="card">
+                <h4 class="card-header">
+                    Create Forum
+                </h4>
+                <div class="card-body">
+                    <form class="form-horizontal" method="post" action="createForumTutId.php">
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="courseName">Choose Course:</label>
+                            <div class="col-sm-10">
+                                <select class="form-control" name="selected" required>
+                                    <option name='coursename' value=''>Select a course</option>
+                                    <?php
+                                    while ($row = pg_fetch_row($result)) {
+                                        echo "<option name='coursename' value='$row[0]'>$row[0]</option>";
+                                    }
+                                    ?>
+
+                                </select>
                             </div>
-                            <input type="submit" name="Action" value="Next">
-                        </form>
-                        <?php 
-                        if (isset($_SESSION['result'])) {
-                            echo $_SESSION['result'];
-                            unset($_SESSION['result']);
-                        }
-                        ?>
-                        <hr>
+                        </div>
                         <br>
-                    </div>
+                        <div class="col-sm-10">
+                            <input type="submit" name="Action" value="Next" class="btn btn-primary">
+                        </div>
+                    </form>
+                    <?php 
+                    if (isset($_SESSION['result'])) {
+                        echo $_SESSION['result'];
+                        unset($_SESSION['result']);
+                    }
+                    ?>
                 </div>
+            </div>
         </body>
     </html> 
     <?php
